61e0258b928ececd7cb59401638715e4626f7fdd
Jack <jack.branthwaite@gmail.com>
2022-08-15 09:29:01 +1200

    Clear out

--- components/footer/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import s from "./Footer.module.scss";
-
-const Footer = () => {
-  return <div className={s.FooterContainer}>jack branthwaite 2022</div>;
-};
-
-export default Footer;
